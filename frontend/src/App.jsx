892fb64849175ee03a6ae10328500fe976b4e74c
MNaveenKumar2005 <ce22b078@smail.iitm.ac.in>
2025-07-31 15:09:43 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-yellow-200">
+      <h1>
+        Tailwind is Working!
+      </h1>
+      <h1 class="text-red-600 bg-yellow-200 text-3xl">Direct Tailwind Test</h1>
+    </div>
+  )
+}
+
+export default App
+
