989c87c959bedf32e03447c3feadca3924b93900
MNaveenKumar2005 <ce22b078@smail.iitm.ac.in>
2025-08-07 02:43:17 +0530

    .env added

--- frontend/src/auth/firebase.js
+++ frontend/src/auth/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA944D3Xp1CUaK5Pb02V2XbNdNsuGyhwFQ",
-  authDomain: "ask-anna-fef06.firebaseapp.com",
-  projectId: "ask-anna-fef06",
-  storageBucket: "ask-anna-fef06.firebasestorage.app",
-  messagingSenderId: "655848520137",
-  appId: "1:655848520137:web:f5cf5937cba74231c19018",
-  measurementId: "G-6GZN4DZC4M"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
